c7ad664cdd1c8be09c1f3c29eee4393bf7bcc735
siv-the-programmer <sivariobuch@gmail.com>
2025-10-23 20:21:19 +0200

    Made the conditonals.py file much cleaner and neater

--- basics/conditionals.py
+++ basics/conditionals.py
@@ -1,10 +1,8 @@
-#----------Conditionals with Python---------
-print("Lets learn conditionals")
-print("-----------------------")
+"""----------Conditionals with Python---------
 
-# What is conditionals?
+# What are conditionals?
 
-# Conditionals in Python are statements that allow a program to make decisions and execute different blocks of code based on whether a specified condition is true or false.
+# Conditionals in Python are statements that allow a program to make decisions and execute different blocks of code based on whether a specified condition is True or False.
 # They are fundamental for controlling the flow of a program and enabling it to respond dynamically to various inputs or situations.
 
 # Conditionals:
@@ -12,35 +10,44 @@ print("-----------------------")
 # if 
 # elif
 # else
-
-# How they work
-
-#----------------Example 1-----------------------
-
+"""
+"""
+---------------------------
+    How they work Example
+---------------------------
+"""
 # We store the users input in the variable called <name>
 name = input("What is your name: ")
 if name == "John": # if the user name is equal to john we will print a message "Hello john" 
     print(f"Hello {name}")
 else: # If the users name is not john we will print a message letting the program know john is not present
     print(f"I only say hello if john is present")
-    
-#---------------Example 2-----------------------
-
+"""
+---------------------------
+    Example 2
+---------------------------
+"""
 age = input("Enter your age ")
 if age >= "18":
     print("You are allowed to vote")
 else:
     print("Please come back when you are 18 or over") 
 
-#---------------Elif--(elseif)---------------
-
+"""
+---------------------------
+    elif : if else
+---------------------------
 # The elif conditional basically means (else if) , for example: if name is John or Mary or Sam, then do this. If not, do something else
 # We would say (if) your name is John (elif) Mary (elif) Sam then do this or (else) do something else
 # We will also use the statement (or) to make sure it doesnt matter if the first letter is uppercase or lowercase
-
-#----------------Example----------------------
+"""
+"""
+---------------------------
+    elif Example
+---------------------------
+"""
+"""
 # In this example is will be using:
-
 # Variables
 # Print 
 # if 
@@ -48,7 +55,7 @@ else:
 # else
 # or
 # f string
-
+"""
 print("The only people allowed to Access are:\n")
 print("John, Mary, Sam") # We print out the names on the screen for those who are allowed to enter
 j = "John" # We store all the user names in variables so we can use them later
@@ -64,12 +71,16 @@ elif names == "mary" or names == "Mary": # if the name is mary or Mary
 else:
     print("Access Denied") # If none of the names match the names shown above we will then print out Access Denied
     
-#---------------------------Example using Boolean----
+"""
+---------------------------
+    Example using Boolean
+---------------------------
+"""
 print("light on or off?") # print light on or off on the screen for the user to see
 light_on = input("> ") # variable called light_on stores the value or data the user inputs
 if light_on == "on": # if the user types "on" meaning the light is on we will print the bool True
     print(True)
-elif light_on == "off": # if the user types "off" meaning the light is off we will print the bool False
+elif light_on == "off": # if the user types "off" meaning the light is off we will print the Bool False
     print(False)
     
 
